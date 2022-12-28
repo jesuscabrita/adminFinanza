73aaa9ee89cc564998546cde7e59287cd05c2ff7
Jesus Cabrita <jesusarnaldo115@gmail.com>
2022-12-27 23:49:22 -0300

    front:apis

--- frontend/service/api.ts
+++ frontend/service/api.ts
@@ -4,4 +4,4 @@ export const api = axios.create({baseURL:'/'})
 
 export const api2 = axios.create({baseURL:'https://api.bluelytics.com.ar/'})
 
-export const api3 = axios.create({baseURL:"https://backadminfinanza.herokuapp.com/"})
+export const api3 = axios.create({baseURL:"http://localhost:5000"})
